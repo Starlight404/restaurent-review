4df7d1e076602554b127e1cf8ac11e322c30dc71
Starlight404 <rahulkpcaiub@gmail.com>
2022-08-20 19:42:39 +0600

    wip:db setup done

--- backend/api/restaurents.route.js
+++ backend/api/restaurents.route.js
@@ -1,6 +1,6 @@
 import  express  from "express"
 
 const router = express.Router()
-router.route("/api/v1/restaurents").get((req,res) =>res.send("hello world"))
+router.route("/").get((req,res) =>res.send("hello world"))
 
 export default router
